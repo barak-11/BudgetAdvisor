483a488c344fcb597446fc04e4893b8116e76c2b
Barak Schieber <barakse11@gmail.com>
2018-06-02 11:48:47 +0300

    added firebase instance to StatsActivity

--- app/src/main/java/com/budgetadviser/android/budgetadvisor/StatsActivity.java
+++ app/src/main/java/com/budgetadviser/android/budgetadvisor/StatsActivity.java
@@ -1,17 +1,42 @@
 package com.budgetadviser.android.budgetadvisor;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class StatsActivity extends AppCompatActivity {
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private List<Purchase> list_purchases = new ArrayList<>();
+    private ProgressBar circular_progress;
+    private GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +53,104 @@ public class StatsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
+
+        circular_progress = (ProgressBar)findViewById(R.id.circular_progress);
+        graph = (GraphView) findViewById(R.id.graph);
+
+
+        try {
+            initFirebase();
+            addEventFirebaseListener();
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
 
 
 
     }
+    private void initFirebase() {
+        FirebaseApp.initializeApp(this);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference  = mFirebaseDatabase.getReference();
+    }
+    private void addEventFirebaseListener() {
+        //Progressing
+        graph.setVisibility(View.INVISIBLE);
+        circular_progress.setVisibility(View.VISIBLE);
+
+
+        mDatabaseReference.child("purchase").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                try {
+                    if (list_purchases.size() > 0)
+                        list_purchases.clear();
+
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                        //String date_str = postSnapshot.child("date").toString();
+                        Purchase purchase = new Purchase(postSnapshot.child("name").getValue().toString(), postSnapshot.child("uid").getValue().toString(), Integer.valueOf(postSnapshot.child("price").getValue().toString()), postSnapshot.child("address").getValue().toString(),postSnapshot.child("date").getValue().toString());
+                        list_purchases.add(purchase);
+                    }
+                    //System.out.println("list_purchases is:"+list_purchases.size());
+
+                    // generate Dates
+                    Calendar calendar = Calendar.getInstance();
+                    //System.out.println("calendar is:"+calendar);
+                    //System.out.println("getTime() is:"+calendar.getTime());
+                    Date d1 = calendar.getTime();
+                    calendar.add(Calendar.DATE, 1);
+                    Date d2 = calendar.getTime();
+                    calendar.add(Calendar.DATE, 1);
+                    Date d3 = calendar.getTime();
+
+
+
+// you can directly pass Date objects to DataPoint-Constructor
+// this will convert the Date to double via Date#getTime()
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                            new DataPoint(d1, 1),
+                            new DataPoint(d2, 5),
+                            new DataPoint(d3, 3)
+                    });
+
+                    graph.addSeries(series);
+
+// set date label formatter
+                    graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(StatsActivity.this));
+                    graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
+
+// set manual x bounds to have nice steps
+                    graph.getViewport().setMinX(d1.getTime());
+                    graph.getViewport().setMaxX(d3.getTime());
+                    graph.getViewport().setXAxisBoundsManual(true);
+
+// as we use dates as labels, the human rounding to nice readable numbers
+// is not necessary
+                    graph.getGridLabelRenderer().setHumanRounding(false);
+
+
+
+                    circular_progress.setVisibility(View.INVISIBLE);
+                    graph.setVisibility(View.VISIBLE);
+
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "onDataChange() error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+                    Log.e("Budget", "onDataChange() error", e);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
